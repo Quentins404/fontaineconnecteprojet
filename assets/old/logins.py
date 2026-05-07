034906a9ed950c2f0fcd6d76c32d56dac1f8c3f5
Quentins845 <138402819+Quentins404@users.noreply.github.com>
2026-05-07 08:57:13 +0200

    Update logins.py

--- assets/old/logins.py
+++ assets/old/logins.py
@@ -4,7 +4,7 @@
 # ⚠️ NE PAS VERSIONNER CE FICHIER
 # ==========================================================
 
-MYSQL_HOST = "77.37.122.204"        # Adresse serveur MySQL / phpMyAdmin
-MYSQL_DATABASE = "stockage_qr_code"     # Nom de la base de données
-MYSQL_USER = "donnees_projet_fontaine"    # Utilisateur MySQL
-MYSQL_PASSWORD = "CfN7a0fFa5TtuCr7"  # Mot de passe MySQL
+MYSQL_HOST = "IP bdd"        # Adresse serveur MySQL / phpMyAdmin
+MYSQL_DATABASE = "NOM BDD"     # Nom de la base de données
+MYSQL_USER = "USERBDD"    # Utilisateur MySQL
+MYSQL_PASSWORD = "MDP BDD"  # Mot de passe MySQL
